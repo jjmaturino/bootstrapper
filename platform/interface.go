ecb9832e771506c994f850fb29df413a909112ed
Juan Maturino <juanjmaturino@gmail.com>
2025-02-28 11:56:51 -0600

    feature: included interface for service runtime considerations

--- platform/interface.go
+++ platform/interface.go
@@ -3,23 +3,25 @@ package platform
 import (
 	"context"
 	"github.com/gin-gonic/gin"
-	"github.com/samber/do"
 )
 
+// Engine is an interface for HTTP engines like Gin
 type Engine interface {
 	Run(addr ...string) (err error)
-	Handle(method, relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
+	Handle(method, relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes // TODO: Generalize this, Only allows for gin
 }
 
-type Service interface {
-	ConstructService(ctx context.Context, injector *do.Injector) error
-	SetupEngine(engine Engine) (Engine, error)
-}
-
+// ApiService defines the interface that all services must adhere to
 type ApiService interface {
-	ConstructService(ctx context.Context, deps ...interface{}) error
+	ConstructService(ctx context.Context, deps ...interface{}) error // TODO: Potentiall add ability to pass something like deps injector from do'samber
 	// SetupEngine is called after ConstructServices. The Service is expected to bind any routes or other
 	// necessary work to setup provided Engine for initial use. Usually this just means binding any appropriate
 	// routes to handlers.
 	SetupEngine(eng Engine) (Engine, error)
+}
+
+// RuntimeStarter defines how to start a service on a specific runtime
+type RuntimeStarter interface {
+	// Start begins the service on the specific runtime
+	Start(service ApiService, engine Engine, deps ...interface{}) error
 }
