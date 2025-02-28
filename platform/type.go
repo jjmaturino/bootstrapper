8cb70dc2a7b639d527e7a79eaac38693405a16a5
Juan Maturino <juanjmaturino@gmail.com>
2025-02-28 11:57:30 -0600

    feature: added consts for future platforms supported

--- platform/type.go
+++ platform/type.go
@@ -2,7 +2,15 @@ package platform
 
 type Type string
 
-const VM Type = "virtual_machine"
+// Platform environment constants
+const (
+	VM Type = "virtual_machine" // Only implementing VM for now
+
+	// Future platform types (placeholders)
+	// Docker      Type = "docker"
+	// Lambda      Type = "lambda"
+	// Kubernetes  Type = "kubernetes"
+)
 
 func (pT *Type) String() string {
 	return string(*pT)
