408903d447f64e1a95fd1a2b3d59e968ef0ebebb
Juan Maturino <juanjmaturino@gmail.com>
2025-03-06 18:59:21 -0600

    test: updated tests

--- platform/type_test.go
+++ platform/type_test.go
@@ -7,6 +7,15 @@ func TestType(t *testing.T) {
 	expected := "virtual_machine"
 
 	if pT.String() != expected {
-		t.Errorf("Expected %s, got %s", "virtual_machine", pT.String())
+		t.Errorf("Expected %s, got %s", expected, sT.String())
+	}
+}
+
+func TestServiceType(t *testing.T) {
+	sT := HTTPServiceType
+	expected := "http"
+
+	if sT.String() != expected {
+		t.Errorf("Expected %s, got %s", expected, sT.String())
 	}
 }
